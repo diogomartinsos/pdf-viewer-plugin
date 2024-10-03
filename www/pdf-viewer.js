3cfc9b86a88e5bcd4298626408889e04b00d7f2f
Diogo Martins <diogo.martins@outsystems.com>
2024-10-03 15:56:17 +0100

    fixed function name in js call

--- www/pdf-viewer.js
+++ www/pdf-viewer.js
@@ -2,5 +2,5 @@ var exec = require("cordova/exec");
 var PLUGIN_NAME = "PDFViewer";
 
 exports.loadPDFFromURL = function (success, error, args) {
-	exec(success, error, PLUGIN_NAME, "echo", [args]);
+	exec(success, error, PLUGIN_NAME, "loadPDFfromURL", [args]);
 };
