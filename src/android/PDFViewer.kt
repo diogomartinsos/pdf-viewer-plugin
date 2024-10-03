11abfd310ce55666708c627faff07e0755546c02
Diogo Martins <diogo.martins@outsystems.com>
2024-10-03 15:21:40 +0100

    first commit

---
+++ src/android/PDFViewer.kt
@@ -0,0 +1,39 @@
+package com.outsystems.experts
+
+import android.util.Log
+import com.rajat.pdfviewer.PdfViewerActivity
+import com.rajat.pdfviewer.util.saveTo
+import org.apache.cordova.CallbackContext
+import org.apache.cordova.CordovaInterface
+import org.apache.cordova.CordovaPlugin
+import org.apache.cordova.CordovaWebView
+import org.apache.cordova.PluginResult
+import org.json.JSONArray
+import org.json.JSONException
+
+class PDFViewer : CordovaPlugin() {
+    companion object {
+        private const val TAG = "PDFViewer"
+    }
+
+    override fun initialize(cordova: CordovaInterface, webView: CordovaWebView) {
+        super.initialize(cordova, webView)
+        Log.d(TAG, "Initializing MyCordovaPlugin")
+    }
+
+    @Throws(JSONException::class)
+    override fun execute(action: String, args: JSONArray, callbackContext: CallbackContext): Boolean {
+        if (action == "loadPDFfromURL") {
+            PdfViewerActivity.launchPdfFromUrl(
+                context = this.cordova.context, // Replaced with appropriate context
+                pdfUrl = args.getString(0),
+                pdfTitle = "PDF Title",
+                saveTo = saveTo.ASK_EVERYTIME, // Replace SaveTo with the actual enum or class used
+                enableDownload = true
+            ).also { intent -> 
+                cordova.activity.startActivity(intent)
+            }
+        }
+        return true
+    }
+}
---
+++ www/pdf-viewer.js
@@ -0,0 +1,6 @@
+var exec = require("cordova/exec");
+var PLUGIN_NAME = "PDFViewer";
+
+exports.loadPDFFromURL = function (success, error, args) {
+	exec(success, error, PLUGIN_NAME, "echo", [args]);
+};
